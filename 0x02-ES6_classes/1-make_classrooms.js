699c349d4f5857e79cb961dfb47d1475672635e5
ShogoMark <trigmark87@gmail.com>
2023-06-29 22:17:13 +0100

    task1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './0-classroom';
+
+// eslint-disable-next-line no-unused-vars
+function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
