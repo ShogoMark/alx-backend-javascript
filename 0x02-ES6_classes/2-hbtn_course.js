550dabf7b9217cad7bd3880d0198e4b2cc46c076
ShogoMark <trigmark87@gmail.com>
2023-06-30 01:09:14 +0100

    task3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,7 +8,7 @@ export default class HolbertonCourse {
       throw new TypeError('Length must be a number');
     }
 
-    if (!Array.isArray(students)) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array');
     }
 
@@ -22,6 +22,9 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
@@ -41,6 +44,10 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
+    set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = value;
   }
 }
