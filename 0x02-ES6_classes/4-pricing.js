b8671bb5cc8f7ecac06f23235f21319171ef0799
ShogoMark <trigmark87@gmail.com>
2023-06-30 05:08:41 +0100

    task4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,39 +1,39 @@
-import Currency from './3-currency'
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
-  
+
   get amount() {
     return this._amount;
   }
 
   set amount(value) {
-    if (typeof value != 'number') {
+    if (typeof value !== 'number') {
       throw new TypeError('amount must be a number');
     }
-    this._amount = value
+    this._amount = value;
   }
-  
+
   get currency() {
     return this._currency;
   }
 
   set currency(value) {
-   if (typeof value != 'string') {
+    if (typeof value !== 'string') {
       throw new TypeError('currency must be a string');
     }
-    this._currency = value 
+    this._currency = value;
   }
-  
+
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`
-}
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate
- }
+    return amount * conversionRate;
+  }
 }
-
