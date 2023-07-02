3c50524f068b54cd5123444110497a7041794a73
ShogoMark <trigmark87@gmail.com>
2023-07-02 16:38:43 +0100

    six

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,6 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
     }
     super(sqft);
     this._floors = floors;
