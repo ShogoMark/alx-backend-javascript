3bcc7ea10e926297bebc6d121ed092b9a17964a0
ShogoMark <trigmark87@gmail.com>
2023-07-02 15:13:09 +0100

    task5s

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,17 +2,13 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
-
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
- }
+}
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
+  static evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
