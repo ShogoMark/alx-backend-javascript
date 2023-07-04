337ec6b275233fffa118ae39b0f9dc6f855936d9
ShogoMark <trigmark87@gmail.com>
2023-07-04 21:48:26 +0100

    task2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,12 @@
+const getStudentsByLocation = (data, city) => {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  const studentsFiltered = students.filter((student) => student.location === city);
+  return studentsFiltered;
+};
+
+export default getStudentsByLocation;
