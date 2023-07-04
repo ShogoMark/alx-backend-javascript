a7362498b5e9e4e7de907ec7c04d5dfdcbaadaa4
ShogoMark <trigmark87@gmail.com>
2023-07-04 16:57:35 +0100

    task1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,14 @@
+const getListStudentIds = (arr) => {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  const studentIds = students.map((student) => student.id);
+  return studentIds;
+};
+
+export default getListStudentIds;
