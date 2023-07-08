0c5cec0cebf46ecf6335710e996a508c7ec163cc
ShogoMark <trigmark87@gmail.com>
2023-07-08 16:01:27 +0100

    task3s

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 import getListStudentIds from './1-get_list_student_ids';
 
-const getStudentIdsSum = (data) => {
+const getStudentIdsSum = () => {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
@@ -8,8 +8,8 @@ const getStudentIdsSum = (data) => {
   ];
 
   const _ids = getListStudentIds(students);
-  const Id_sum = _ids.reduce((accumulator, currentValue));
-  return Id_sum;
+  const IdSum = _ids.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return IdSum;
 };
 
 export default getStudentIdsSum;
