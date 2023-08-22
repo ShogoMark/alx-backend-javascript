f2e2afa438e03a6452cc3b6a3e7189f16207fc0b
ShogoMark <trigmark87@gmail.com>
2023-08-22 17:06:17 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,28 @@
 const readline = require('readline');
 const { spawn } = require('child_process');
 
-console.log("Welcome to Holberton School, what is your name?");
+function runProgram() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+  console.log("Welcome to Holberton School, what is your name?");
 
-rl.on('line', (name) => {
-  if (name.toLowerCase() === 'exit') {
-    console.log("This important software is now closing");
-    rl.close();
-  } else {
-    const ls = spawn('ls', ['-lh', '/usr']);
-    ls.stdout.on('data', (data) => {
-      console.log(`Your name is: ${name}`);
+  rl.on('line', (name) => {
+    if (name.toLowerCase() === 'exit') {
+      console.log("This important software is now closing");
+      rl.close();
+    } else {
+      const ls = spawn('ls', ['-lh', '/usr']);
+        ls.stdout.on('data', (data) => {
+        console.log(`Your name is: ${name}`);
     });
   }
 });
+
+module.exports = runProgram;
+
+if (require.main === module) {
+  runProgram();
+}
