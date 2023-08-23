4c9e3201b54160931df507b50fc36d197fdbc943
ShogoMark <trigmark87@gmail.com>
2023-08-23 09:36:54 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,4 @@
 const readline = require('readline');
-const { spawn } = require('child_process');
 
 function runProgram() {
   const rl = readline.createInterface({
@@ -9,19 +8,24 @@ function runProgram() {
 
   console.log('Welcome to Holberton School, what is your name?');
 
-  rl.on('line', (name) => {
-    if (name.toLowerCase() === 'exit') {
-      console.log('This important software is now closing');
+  if (process.stdin.isTTY) {
+    rl.on('line', (name) => {
+      if (name.trim() === '') {
+        console.log('This important software is now closing');
+        rl.close();
+      } else {
+        console.log(`Your name is: ${name}`);
+        console.log("This important software is now closing");
+        rl.close();
+      }
+    });
+  } else {
+    rl.on('line', (name) => {
+      console.log(`Your name is: ${name}`);
+      console.log("This important software is now closing");
       rl.close();
-    } else {
-      const ls = spawn('ls', ['-lh', '/usr']);
-      // eslint-disable-next-line no-unused-vars
-      ls.stdout.on('data', (data) => {
-        console.log(`Your name is: ${name}\r`);
-        console.log(data.toString());
-      });
-    }
-  });
+    });
+  }
 }
 
 module.exports = runProgram;
