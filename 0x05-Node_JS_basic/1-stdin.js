26c5b34dec109484134d4824ffd48353e7fbbc6f
ShogoMark <trigmark87@gmail.com>
2023-08-23 11:54:28 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,35 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-function runProgram() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-  console.log('Welcome to Holberton School, what is your name?');
-
-  if (process.stdin.isTTY) {
-    rl.on('line', (name) => {
-      if (name.trim() === '') {
-        console.log('This important software is now closing');
-        rl.close();
-      } else {
-        console.log(`Your name is: ${name}`);
-        console.log('This important software is now closing');
-        rl.close();
-      }
-    });
-  } else {
-    rl.on('line', (name) => {
-      console.log(`Your name is: ${name}`);
-      console.log('This important software is now closing');
-      rl.close();
-    });
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
-}
+});
 
-module.exports = runProgram;
-
-if (require.main === module) {
-  runProgram();
-}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
