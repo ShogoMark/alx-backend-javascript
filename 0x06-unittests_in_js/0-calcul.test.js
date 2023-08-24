9eeb9f7db4dc7d7a9bd2637ac72da0fe05d92fcd
ShogoMark <trigmark87@gmail.com>
2023-08-24 10:44:21 +0100

    task0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,13 +3,23 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
   it('should round and sum two positive number', function() {
-    const result = calculateNumber(2.7, 5.2);
-    assert.strictEqual(result, 8);
+    const result = calculateNumber(1, 3);
+    assert.strictEqual(result, 4);
   });
 
   it('should round and sum a positive and a negative number', function() {
-    const result = calculateNumber(3.2, -4.6);
-    assert.strictEqual(result, -2);
+    const result = calculateNumber(1, 3.7);
+    assert.strictEqual(result, 5);
+  });
+
+  it('should round and sum two floats', function() {
+    const result = calculateNumber(1.2, 3.7);
+    assert.strictEqual(result, 5);
+  });
+
+  it('should round and sum two negative floats', function() {
+    const result = calculateNumber(-1.2, -3.7);
+    assert.strictEqual(result, -5);
   });
 
   it('should round and sum zero and one', function() {
